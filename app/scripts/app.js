89181959d757ccabb84829c6f7a625fe537e2f45
naveendnagendran <naveendnagendran@gmail.com>
2015-10-26 13:50:51 -0700

    Final

---
+++ app/scripts/app.js
@@ -0,0 +1,41 @@
+(
+
+
+
+  function(){
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pnChatApp
+ * @description
+ * # yoChatApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('pnChatApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'pubnub.angular.service'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/join', {
+        templateUrl: 'views/join.html',
+        controller: 'JoinCtrl'
+      })
+      .otherwise({
+        redirectTo: '/join'
+      });
+  });
+
+}());
